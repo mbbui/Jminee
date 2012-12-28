5a6ff848d1cf92d5e3d0fc62609537644108b9b9
Bach Bui <bachbui@gmail.com>
2012-12-28 16:14:40 -0600

    update databases

--- database/jminee.sql
+++ database/jminee.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.22, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.28, for osx10.7 (i386)
 --
 -- Host: localhost    Database: jminee
 -- ------------------------------------------------------
--- Server version	5.5.22-0ubuntu1
+-- Server version	5.5.28
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,31 +16,94 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `member_message`
+-- Table structure for table `comment`
 --
 
-DROP TABLE IF EXISTS `member_message`;
+DROP TABLE IF EXISTS `comment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `member_message` (
-  `user_name` varchar(255) NOT NULL,
-  `message_id` int(11) NOT NULL,
-  `read` tinyint(1) DEFAULT NULL,
-  `delete` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`user_name`,`message_id`),
-  KEY `message_id` (`message_id`),
-  CONSTRAINT `member_message_ibfk_1` FOREIGN KEY (`user_name`) REFERENCES `tg_user` (`user_name`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `member_message_ibfk_2` FOREIGN KEY (`message_id`) REFERENCES `message` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `comment` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `time` datetime NOT NULL,
+  `content` varchar(5000) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `creator_id` int(11) NOT NULL,
+  `deleted` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`uid`),
+  KEY `subject_id` (`subject_id`),
+  KEY `creator_id` (`creator_id`),
+  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`uid`),
+  CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`creator_id`) REFERENCES `tg_user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `member_message`
+-- Dumping data for table `comment`
 --
 
-LOCK TABLES `member_message` WRITE;
-/*!40000 ALTER TABLE `member_message` DISABLE KEYS */;
-/*!40000 ALTER TABLE `member_message` ENABLE KEYS */;
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+INSERT INTO `comment` VALUES (2,'2012-12-26 23:53:13','This hw due in 12/12/12',10,5,0),(3,'2012-12-26 23:53:13','This hw due in 12/30/12',11,5,0),(4,'2012-12-27 00:19:44','On 12/30/12',12,5,0),(5,'2012-12-27 01:02:18','Due on 01/12',13,5,0),(6,'2012-12-27 01:02:18','Due on 02/13',14,5,0),(7,'2012-12-27 01:02:18','Due on 02/24',15,5,0),(8,'2012-12-27 01:02:18','Due on 12/04',16,5,0),(9,'2012-12-27 01:02:18','Please see the attachement',17,5,0),(10,'2012-12-28 11:42:09','Is this a 2 hours exam?',10,5,0),(11,'2012-12-28 11:50:11','Yes it is.',10,5,0),(12,'2012-12-28 11:51:42','That is too long, TA!',10,5,0),(13,'2012-12-28 11:52:24','It has been like this forever :)',10,5,0),(14,'2012-12-28 11:55:30','thanks!',10,5,0),(16,'2012-12-28 11:55:30','Another question, when is the due data of the final project?',10,5,0),(17,'2012-12-28 11:55:30','Have you seen my last question?',10,5,0),(18,'2012-12-28 11:55:30','It will be due a week after final.',10,5,0),(19,'2012-12-28 12:37:33','It will be due a week after final.',10,5,0),(20,'2012-12-28 12:47:47','You forgot the attachment.',13,5,0),(21,'2012-12-28 12:53:32','Oops, sorry!',13,5,0),(22,'2012-12-28 14:18:42','How can I submit my HW?',13,5,0),(23,'2012-12-28 14:20:13','You can attach it here.',13,5,0),(24,'2012-12-28 14:20:27','Did you get mine?',13,5,0),(31,'2012-12-28 15:33:16','Please register for the trip.',27,5,0),(32,'2012-12-28 15:34:02','Bach, John, Vinay',28,5,0),(33,'2012-12-28 15:34:14','Hey, I would like to go',27,5,0),(34,'2012-12-28 15:34:50','Me too!',27,5,0);
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comment_subject`
+--
+
+DROP TABLE IF EXISTS `comment_subject`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment_subject` (
+  `comment_id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `deleted` tinyint(1) NOT NULL,
+  PRIMARY KEY (`comment_id`,`subject_id`),
+  KEY `subject_id` (`subject_id`),
+  CONSTRAINT `comment_subject_ibfk_1` FOREIGN KEY (`comment_id`) REFERENCES `comment` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `comment_subject_ibfk_2` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment_subject`
+--
+
+LOCK TABLES `comment_subject` WRITE;
+/*!40000 ALTER TABLE `comment_subject` DISABLE KEYS */;
+INSERT INTO `comment_subject` VALUES (2,10,0),(3,11,0),(4,12,0),(5,13,0),(6,14,0),(7,15,0),(8,16,0),(9,17,0);
+/*!40000 ALTER TABLE `comment_subject` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `member_subject`
+--
+
+DROP TABLE IF EXISTS `member_subject`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `member_subject` (
+  `member_id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `last_read` int(11) NOT NULL,
+  `muted` tinyint(1) NOT NULL,
+  PRIMARY KEY (`member_id`,`subject_id`),
+  KEY `subject_id` (`subject_id`),
+  KEY `last_read` (`last_read`),
+  CONSTRAINT `member_subject_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `tg_user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `member_subject_ibfk_2` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `member_subject_ibfk_3` FOREIGN KEY (`last_read`) REFERENCES `comment` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `member_subject`
+--
+
+LOCK TABLES `member_subject` WRITE;
+/*!40000 ALTER TABLE `member_subject` DISABLE KEYS */;
+INSERT INTO `member_subject` VALUES (5,10,2,0),(5,11,3,0),(5,12,4,0),(5,13,5,0),(5,14,6,0),(5,15,7,0),(5,16,8,0),(5,17,9,0),(5,27,31,0),(5,28,32,0);
+/*!40000 ALTER TABLE `member_subject` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -51,16 +114,17 @@ DROP TABLE IF EXISTS `member_topic`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `member_topic` (
-  `user_name` varchar(255) NOT NULL,
+  `member_id` int(11) NOT NULL,
   `topic_id` int(11) NOT NULL,
   `role` varchar(2) NOT NULL,
   `local_title` varchar(255) NOT NULL,
-  `delete` tinyint(1) DEFAULT '0',
-  PRIMARY KEY (`user_name`,`topic_id`),
+  `deleted` tinyint(1) NOT NULL,
+  `unsubscribed` tinyint(1) NOT NULL,
+  PRIMARY KEY (`member_id`,`topic_id`),
   KEY `topic_id` (`topic_id`),
-  CONSTRAINT `member_topic_ibfk_1` FOREIGN KEY (`user_name`) REFERENCES `tg_user` (`user_name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `member_topic_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `tg_user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `member_topic_ibfk_2` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,40 +133,8 @@ CREATE TABLE `member_topic` (
 
 LOCK TABLES `member_topic` WRITE;
 /*!40000 ALTER TABLE `member_topic` DISABLE KEYS */;
-INSERT INTO `member_topic` VALUES ('bachbui@gmail.com',3,'r','CS431 Database',0),('bachbui@gmail.com',4,'r','CS73 Algorithm',0),('robert.m.pieta@gmail.com',5,'c','Test',0),('robert.m.pieta@gmail.com',7,'r','Hadoop 101',0),('testuser',1,'c','Thiller book club',0),('testuser',2,'c','Soccer club',0),('testuser',3,'c','CS431 Database',0),('testuser',4,'c','CS73 Algorithm',0),('testuser',6,'c','Performance 101',0),('testuser',7,'c','Hadoop 101',0);
+INSERT INTO `member_topic` VALUES (5,16,'c','CS 273 / OS',0,0),(5,17,'c','Hadoop Performance',0,0),(5,18,'c','CS 431 / Real-time Systems',0,0),(5,19,'c','Niagra Fall camping',0,0),(5,20,'c','Dine out Friday with the Gang',0,0),(5,21,'c','Basketball Tournament',0,0),(5,22,'c','\'Web Monkey\'',0,0);
 /*!40000 ALTER TABLE `member_topic` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `message`
---
-
-DROP TABLE IF EXISTS `message`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `message` (
-  `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `time` datetime DEFAULT NULL,
-  `subject` varchar(255) NOT NULL,
-  `content` varchar(5000) DEFAULT NULL,
-  `topic_id` int(11) DEFAULT NULL,
-  `creator_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`uid`),
-  KEY `topic_id` (`topic_id`),
-  KEY `creator_name` (`creator_name`),
-  CONSTRAINT `message_ibfk_1` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`uid`),
-  CONSTRAINT `message_ibfk_2` FOREIGN KEY (`creator_name`) REFERENCES `tg_user` (`user_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `message`
---
-
-LOCK TABLES `message` WRITE;
-/*!40000 ALTER TABLE `message` DISABLE KEYS */;
-INSERT INTO `message` VALUES (1,'2012-06-26 04:19:42','Homework 2','blahblah',3,'testuser'),(2,'2012-07-22 22:04:39','Distributed File System','blahblah',7,'testuser');
-/*!40000 ALTER TABLE `message` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -189,6 +221,37 @@ LOCK TABLES `registration_resetpassword` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `subject`
+--
+
+DROP TABLE IF EXISTS `subject`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `subject` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `time` datetime NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `topic_id` int(11) NOT NULL,
+  `creator_id` int(11) NOT NULL,
+  PRIMARY KEY (`uid`),
+  KEY `topic_id` (`topic_id`),
+  KEY `creator_id` (`creator_id`),
+  CONSTRAINT `subject_ibfk_1` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`uid`),
+  CONSTRAINT `subject_ibfk_2` FOREIGN KEY (`creator_id`) REFERENCES `tg_user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subject`
+--
+
+LOCK TABLES `subject` WRITE;
+/*!40000 ALTER TABLE `subject` DISABLE KEYS */;
+INSERT INTO `subject` VALUES (10,'2012-12-26 23:53:13','Homework 1',16,5),(11,'2012-12-26 23:53:13','Homework 3',16,5),(12,'2012-12-27 00:19:44','Midterm Exam',16,5),(13,'2012-12-27 01:02:18','Homework 4',16,5),(14,'2012-12-27 01:02:18','Homework 5',16,5),(15,'2012-12-27 01:02:18','Homework 6',16,5),(16,'2012-12-27 01:02:18','Final exam',16,5),(17,'2012-12-27 01:02:18','First class material',18,5),(27,'2012-12-28 15:33:16','Registration',19,5),(28,'2012-12-28 15:34:02','Organizer',19,5);
+/*!40000 ALTER TABLE `subject` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tg_group`
 --
 
@@ -255,7 +318,7 @@ CREATE TABLE `tg_permission` (
   `description` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`permission_id`),
   UNIQUE KEY `permission_name` (`permission_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -285,7 +348,7 @@ CREATE TABLE `tg_user` (
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_name` (`user_name`),
   UNIQUE KEY `email_address` (`email_address`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -334,14 +397,15 @@ DROP TABLE IF EXISTS `topic`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `topic` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `time` datetime DEFAULT NULL,
+  `time` datetime NOT NULL,
   `title` varchar(255) NOT NULL,
-  `creator_name` varchar(255) DEFAULT NULL,
-  `update_time` datetime DEFAULT NULL,
+  `creator_id` int(11) NOT NULL,
+  `update_time` datetime NOT NULL,
+  `logourl` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`uid`),
-  KEY `creator_name` (`creator_name`),
-  CONSTRAINT `topic_ibfk_1` FOREIGN KEY (`creator_name`) REFERENCES `tg_user` (`user_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  KEY `creator_id` (`creator_id`),
+  CONSTRAINT `topic_ibfk_1` FOREIGN KEY (`creator_id`) REFERENCES `tg_user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -350,7 +414,7 @@ CREATE TABLE `topic` (
 
 LOCK TABLES `topic` WRITE;
 /*!40000 ALTER TABLE `topic` DISABLE KEYS */;
-INSERT INTO `topic` VALUES (1,'2012-06-26 04:06:51','Thiller book club','testuser',NULL),(2,'2012-06-26 04:08:33','Soccer club','testuser',NULL),(3,'2012-06-26 04:10:55','CS431 Database','testuser',NULL),(4,'2012-06-26 04:11:21','CS73 Algorithm','testuser',NULL),(5,'2012-07-18 22:21:09','Test','robert.m.pieta@gmail.com',NULL),(6,'2012-07-22 21:31:29','Performance 101','testuser',NULL),(7,'2012-07-22 21:33:53','Hadoop 101','testuser',NULL);
+INSERT INTO `topic` VALUES (16,'2012-12-26 13:08:02','CS 273 / OS',5,'2012-12-28 14:51:36','https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSPsE4gdHg-43dGYHCa23nxio5xlFQr6dOguD9gPCAoO7BjMhndJw'),(17,'2012-12-26 13:08:02','Hadoop Performance',5,'2012-12-26 13:08:02','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNzRJoy8K6auu9JKpm6gRwi73Pg_8eFhX8OtYGvPFL5CDb_fmi'),(18,'2012-12-26 13:08:02','CS 431 / Real-time Systems',5,'2012-12-27 01:02:18','https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRDnYoiJs3iQEmraUXuu0HGQmPPgEIFCL8Bqk3uFrLljkUMMOQ1tg'),(19,'2012-12-26 13:08:02','Niagra Fall camping',5,'2012-12-28 15:34:50','https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR9GBLKZ1rHDb2nnOoUAC7_0IE2ufGy7lMAgCcF_jX2Oey_d8zKxQ'),(20,'2012-12-26 13:08:02','Dine out Friday with the Gang',5,'2012-12-26 13:08:02','https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTIDdbe_jrejaj07KjPS5OUODJUBdJUhyDrDeIycrA7WHBJBGPi'),(21,'2012-12-26 13:08:02','Basketball Tournament',5,'2012-12-26 13:08:02','https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSAJX5LHy7O-y4DVwMqVrSd8fbdC0nvmHK-utkf9nd2o2Ax5Fc5'),(22,'2012-12-26 13:54:23','Web Monkey',5,'2012-12-26 13:54:23','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuHie03GOLozVhuQBMoBQRlYDpkmGkJz4PBvqIrTK7vOprJnmnaw');
 /*!40000 ALTER TABLE `topic` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -363,4 +427,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-12-22 19:29:26
+-- Dump completed on 2012-12-28 16:14:08
